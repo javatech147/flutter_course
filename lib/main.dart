73ae92c2279edfa2464d548ae78fe18a73985892
Manish Kumar <javatech147@gmail.com>
2020-09-13 19:38:47 +0530

    Quiz App

--- lib/main.dart
+++ lib/main.dart
@@ -17,21 +17,45 @@ class _MyApp extends State<MyApp> {
   final _questions = const [
     {
       'questionText': 'What is your Favorite Color ?',
-      'answers': ['Black', 'Red', 'Green', 'Blue']
+      'answers': [
+        {'text': 'Black', 'score': 10},
+        {'text': 'Red', 'score': 8},
+        {'text': 'Green', 'score': 4},
+        {'text': 'Blue', 'score': 2}
+      ]
     },
     {
       'questionText': 'What is your Favorite Animal ?',
-      'answers': ['Rabbit', 'Snack', 'Elephant', 'Lion']
+      'answers': [
+        {'text': 'Rabbit', 'score': 4},
+        {'text': 'Snack', 'score': 6},
+        {'text': 'Elephant', 'score': 8},
+        {'text': 'Lion', 'score': 5}
+      ]
     },
     {
       'questionText': 'Who is your favorite Cricketer ?',
-      'answers': ['Dhoni', 'Raina', 'Sachin', 'Sehwag']
+      'answers': [
+        {'text': 'Dhoni', 'score': 10},
+        {'text': 'Raina', 'score': 5},
+        {'text': 'Sachin', 'score': 3},
+        {'text': 'Sehwag', 'score': 8}
+      ]
     },
   ];
 
   var _questionIndex = 0;
+  var _totalScore = 0;
 
-  void _answerQuestion() {
+  void _resetQuiz() {
+    setState(() {
+      _questionIndex = 0;
+      _totalScore = 0;
+    });
+  }
+
+  void _answerQuestion(int score) {
+    _totalScore += score;
     setState(() {
       _questionIndex = _questionIndex + 1;
     });
@@ -49,16 +73,18 @@ class _MyApp extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: Scaffold(
-            appBar: AppBar(
-              title: Text('My First App'),
-            ),
-            body: hasMoreQuestions() // Ternary operator
-                ? Quiz(
-                    questions: _questions,
-                    answerQuestion: _answerQuestion,
-                    questionIndex: _questionIndex,
-                  )
-                : Result()), );
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+          appBar: AppBar(
+            title: Text('My First App'),
+          ),
+          body: hasMoreQuestions() // Ternary operator
+              ? Quiz(
+                  questions: _questions,
+                  answerQuestion: _answerQuestion,
+                  questionIndex: _questionIndex,
+                )
+              : Result(_totalScore, _resetQuiz)),
+    );
   }
 }
--- lib/result.dart
+++ lib/result.dart
@@ -1,14 +1,39 @@
 import 'package:flutter/material.dart';
 
 class Result extends StatelessWidget {
+  final int resultScore;
+  final Function resetQuiz;
+
+  Result(this.resultScore, this.resetQuiz);
+
+  //Creating getter
+  String get resultPhrase {
+    String resultText;
+    if (resultScore <= 12) {
+      resultText = "You are awesome and innocent!!";
+    } else if (resultScore <= 16) {
+      resultText = "You are likeable !!";
+    } else if (resultScore <= 20) {
+      resultText = "You are Strange ..";
+    } else {
+      resultText = "You are so bad.";
+    }
+    return resultText;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Text(
-        'No more questions are present !!',
-        style: TextStyle(
-          fontSize: 24,
-        ),
+      child: Column(
+        children: [
+          Text(
+            resultPhrase,
+            style: TextStyle(
+              fontSize: 24,
+            ),
+          ),
+          FlatButton(onPressed: resetQuiz, child: Text('Restart Quiz'))
+        ],
       ),
     );
   }
